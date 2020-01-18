10dd182fe7637f7e3ad41a74f968c26d0851e639
Bjorn Hjortsberg <bjorn.hjortsberg@gmail.com>
2020-01-18 14:30:12 +0100

    Add remote connection
    Write command output to client
    Read input from client and send to command

--- ExecCommand.java
+++ ExecCommand.java
@@ -1,44 +1,90 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Scanner;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ExecCommand {
 
+    public static Socket startServer(int port) {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            Socket socket = server.accept();
+            return socket;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static InputStream inputStream(Socket socket) {
+        try {
+            InputStream input = socket.getInputStream();
+            return input;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static OutputStream outputStream(Socket socket) {
+        try {
+            OutputStream output = socket.getOutputStream();
+            return output;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     public static void main(String []args) {
-        StringBuffer output = new StringBuffer();
-        String commandStr = args[0];
+        Socket socket = startServer(Integer.parseInt(args[0]));
+        // Read from client
+        InputStream input = inputStream(socket);
+        // Write output to client
+        OutputStream output = outputStream(socket);
+
+        String commandStr = args[1];
         try{
             Process command = Runtime.getRuntime().exec(commandStr);
             // Reading output from Process
             BufferedReader reader = new BufferedReader(
                                         new InputStreamReader(command.getInputStream()));
             // Write data to the Process
-            OutputStream instream = command.getOutputStream();
-            final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(instream));
+            OutputStream commandStream = command.getOutputStream();
+            final BufferedWriter writer = new BufferedWriter(
+                                            new OutputStreamWriter(commandStream));
 
-            Scanner scan = new Scanner(System.in);
 
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    while (command.isAlive()) {
-                        String input = scan.nextLine();
-                        input += "\n";
-                        if (command.isAlive()) {
-                            try {
-                                writer.write(input);
+                    byte[] buffer = new byte[1024];
+                    try {
+                        int bytesRead;
+                        while (command.isAlive() &&
+                                ((bytesRead = input.read(buffer)) != -1))
+                        {
+                            buffer[bytesRead] = '\n';
+                            if (command.isAlive()) {
+                                writer.write(new String(buffer));
                                 writer.flush();
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                                // Sleep to make the command exit
+                                // in case 'exit' was written
+                                Thread.sleep(200);
+                            } else {
+                                break;
                             }
                         }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                    System.out.println("Returning, close thread");
                 }
             });
             thread.start();
@@ -46,6 +92,8 @@ public class ExecCommand {
             String line = "";
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
+                line += "\n";
+                output.write(line.getBytes());
             }
             command.waitFor();
             thread.join();
